7e2eeccc66356fc0100602e32fe4b4b45f94cc0e
David Cavins <david.cavins@gmail.com>
2013-01-22 16:14:15 -0600

    CSS changes to BuddyPress components
    registration and profile form work, spacing of elements o members and
    groups pages.

--- functions.php
+++ functions.php
@@ -193,3 +193,33 @@ function add_buddypress_to_body_tag_classes($classes) {
 	return $classes;
 }
 add_filter('body_class','add_buddypress_to_body_tag_classes');
+
+
+//Remove Gravatars for testing on localhost
+function bp_remove_gravatar ($image, $params, $item_id, $avatar_dir, $css_id, $html_width, $html_height, $avatar_folder_url, $avatar_folder_dir) {
+    //$default = get_stylesheet_directory_uri() .'/_inc/images/bp_default_avatar.jpg';
+    $default = '/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg';
+
+    if( $image && strpos( $image, "gravatar.com" ) ){
+        return '<img src="' . $default . '" alt="avatar" class="avatar" ' . $html_width . $html_height . ' />';
+    } else {
+        return $image;
+    }
+}
+add_filter('bp_core_fetch_avatar', 'bp_remove_gravatar', 1, 9 );
+function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
+	//$default = get_stylesheet_directory_uri() .'/_inc/images/bp_default_avatar.jpg';
+    $default = '/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg';
+    return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
+}
+add_filter('get_avatar', 'remove_gravatar', 1, 5);
+function bp_remove_signup_gravatar ($image) {
+	//$default = get_stylesheet_directory_uri() .'/_inc/images/bp_default_avatar.jpg';
+    $default = '/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg';
+    if( $image && strpos( $image, "gravatar.com" ) ){
+        return '<img src="' . $default . '" alt="avatar" class="avatar" width="150" height="150" />';
+    } else {
+        return $image;
+    }
+}
+add_filter('bp_get_signup_avatar', 'bp_remove_signup_gravatar', 1, 1 );
