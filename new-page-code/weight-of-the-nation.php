42bfe8cab3c443377a94c9b6ea46de060d2f67ce
David Cavins <david.cavins@gmail.com>
2013-02-27 08:58:26 -0600

    Weight of the Nation page conversion

---
+++ new-page-code/weight-of-the-nation.php
@@ -0,0 +1,37 @@
+<img src="http://www.communitycommons.org/wp-content/uploads/2012/04/weight-nation-large-1024x543.jpg" alt="Weight of the Nation" title="Weight of the Nation" width="584" height="309" class="wotn-banner aligncenter size-large wp-image-8823" />
+<h3 class="rule-above aligncenter">Want to host a Weight of the Nation event in your community?</h3>
+<div><!-- This is the container that holds the "one-third block" divs. -->
+<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8576" rel="attachment wp-att-8576"><img class=" wp-image-8576  " title="WOTN_220-180" src="http://www.communitycommons.org/wp-content/uploads/2012/04/WOTN_220-180-128x128.jpg" alt="" width="128" height="128" /></a><p>Find community partnerships in your area.</p>
+	</div>
+<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8576" rel="attachment wp-att-8576"><img class=" wp-image-8576  " title="WOTN_220-180" src="http://www.communitycommons.org/wp-content/uploads/2012/04/WOTN_220-180-128x128.jpg" alt="" width="128" height="128" /></a><p>Access the HBO screening kit: Order it now!</p>
+	</div>
+	<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8688" rel="attachment wp-att-8688"><img class="alignnone size-thumbnail wp-image-8688" title="wotn Seal" src="http://www.communitycommons.org/wp-content/uploads/2012/04/wotn-Seal-128x128.gif" alt="" width="128" height="128" /></a><p>Hosting an HBO screening? Download the community activation kit.</p>
+	</div>
+</div>
+<h3 class="rule-above aligncenter">Understand the drivers of obesity in your community, county and state.</h3>
+<div><!-- This is the container that holds the "one-third block" divs. -->
+<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8677" rel="attachment wp-att-8677"><img class="alignnone  wp-image-8677" title="Food Access Map" src="http://www.communitycommons.org/wp-content/uploads/2012/04/Food-Access-Map-128x128.png" alt="" width="128" height="128" /></a><p>Food access map</p>
+	</div>
+	<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8679" rel="attachment wp-att-8679"><img class="alignnone  wp-image-8679" title="Recreation Access Map" src="http://www.communitycommons.org/wp-content/uploads/2012/04/Recreation-Access-Map-128x128.png" alt="" width="128" height="128" /></a><p>Recreation access map</p>
+	</div>
+<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8678" rel="attachment wp-att-8678"><img class="alignnone  wp-image-8678" title="Health Care Map" src="http://www.communitycommons.org/wp-content/uploads/2012/04/Health-Care-Map-128x128.png" alt="" width="128" height="128" /></a><p>Health care access map</p>
+	</div>
+</div>
+<h3 class="rule-above aligncenter">Connect and join the discussion.</h3>
+<div> <!-- This is the container that holds the "one-third block" divs. -->
+<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8602" rel="attachment wp-att-8602"><img class=" wp-image-8602 " title="WON_logo_200" src="http://www.communitycommons.org/wp-content/uploads/2012/04/WON_logo_2002.jpg" alt="" width="128" height="128" /></a><p>2012 Conference</p>
+	</div>
+	<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8676" rel="attachment wp-att-8676"><img class="alignnone  wp-image-8676" title="TWOTN_tease_v4" src="http://www.communitycommons.org/wp-content/uploads/2012/04/TWOTN_tease_v4-128x128.jpg" alt="" width="128" height="128" /></a><p>Join the dialogue and get tips for hosting a local event.</p>
+	</div>
+	<div class="column1of3">
+	<a href="http://commons.local/?attachment_id=8692" rel="attachment wp-att-8692"><img class="alignnone size-thumbnail wp-image-8692" title="Print" src="http://www.communitycommons.org/wp-content/uploads/2012/04/CC-Logo-128x128.jpg" alt="" width="128" height="128" /></a><p>Sign up for dialogue coaching and peer-learning webinars.</p>
+	</div>
+</div>
