52a597b3e68cfdb03bea2bba3ed366cd7f114a22
Michael Barbaro <barbarom@missouri.edu>
2013-03-21 12:57:35 -0500

    Geography ajax transfer from MB localhost

--- ajax/geography.php
+++ ajax/geography.php
@@ -1,93 +1,56 @@
 <?php
-//$mysql_hostname = "localhost";
-//$mysql_user = "root";
-//$mysql_password = "mrt7777!";
-//$mysql_database = "cim2data";
-//$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) 
-//or die("Oops some thing went wrong");
-//mysql_select_db($mysql_database, $bd) or die("Oops some thing went wrong");
-//
-//if($_POST['id'])
-//{
-//    $thisid=$_POST['id'];
-//
-//    $sql = mysql_query("SELECT STFID,CNTYNAME FROM cim2data.counties WHERE StateName = '" . $thisid . "' Order By CntyName", $bd);
-//
-//    while($row=mysql_fetch_array($sql))
-//    {
-//        $id=$row['STFID'];
-//        $data=$row['CNTYNAME'];
-//        echo '<option value="'.$id.'">'.$data.'</option>';
-//    }
-//}
-//if($_POST['id2'])
-//{
-//    $thisid2=$_POST['id2'];
-//
-//    $sql2 = mysql_query("SELECT FIPS,STATENAME,CITYNAME FROM cim2data.cities WHERE StateName = '" . $thisid2 . "' Order By CityName", $bd);
-//
-//    while($row2=mysql_fetch_array($sql2))
-//    {
-//        $id2=$row2['FIPS'];
-//        $data2=$row2['CITYNAME'];
-//        echo '<option value="'.$id2.'">'.$data2.'</option>';
-//    }
-//}
 
 define('WP_USE_THEMES', false);
 require_once($_SERVER['DOCUMENT_ROOT'] . '/wordpress/wp-blog-header.php');
 
-if($_POST['id'])
-{
-    $thisid = 'counties-' . $_POST['id'];
-    
-    $cntyterm = get_term_by('slug', $thisid, 'geographies'); 
-    $tid = $cntyterm->term_id;
-    
-	$args = array(
-		'parent' => $tid,
-		'hide_empty' => 0,
+$selstate = $_POST['selstate'];
+$geog = $_POST['geog'];
+$geogstr = "";
 
-	);
-	$terms = get_terms( 'geographies', $args );
-	
-	if ( $terms ) {
-
-		foreach ( $terms as $term ) {
-			printf( '<option value="%s">%s</option>', $term->slug, $term->name );
-		}
-
-	}
+ if ($geog === 'County') {     
+     $geogstr="counties-";
+ }
+ 
+if ($geog === 'City') {     
+     $geogstr="cities-";
+ }
+ 
+if ($geog === 'School District') {     
+    $geogstr="schooldistricts-";
 }
+ 
+if ($geog === 'US Congressional District') {     
+    $geogstr="uscongressionaldistricts-";
+ }
+ 
+if ($geog === 'State House District') {     
+    $geogstr="statehousedistricts-";
+ }
+ 
+if ($geog === 'State Senate District') {     
+    $geogstr="statesenatedistricts-";
+ }
 
-if($_POST['id2'])
+
+
+if($selstate)
 {
-    $thisid2 = 'cities-' . $_POST['id2'];
-    
-    $cntyterm = get_term_by('slug', $thisid2, 'geographies'); 
-    $tid = $cntyterm->term_id;
-    
-	$args = array(
-		'parent' => $tid,
-		'hide_empty' => 0,
-
-	);
-	$terms = get_terms( 'geographies', $args );
-	
-	if ( $terms ) {
-
-		foreach ( $terms as $term ) {
-			printf( '<option value="%s">%s</option>', $term->slug, $term->name );
-		}
-
-	}
+    if ($geog) {     
+        $thisid = $geogstr . $selstate;
+        $geoterm = get_term_by('slug', $thisid, 'geographies'); 
+        $tid = $geoterm->term_id;
+            $args = array(
+                    'parent' => $tid,
+                    'hide_empty' => 0,
+            );
+            $terms = get_terms( 'geographies', $args );
+            if ( $terms ) {                    
+                    foreach ( $terms as $term ) {
+                            printf( '<option value="' . $term->name . '">' . $term->name . '</option>' );
+                    }
+            }
+    }
 }
-
-
-
-
-
-
 
 
 
