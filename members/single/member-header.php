cc1b2a9101a43a4bf4ccc8477eac195cc2af08d2
David Cavins <david.cavins@gmail.com>
2013-03-22 14:29:30 -0500

    Mobile first BP area

--- members/single/member-header.php
+++ members/single/member-header.php
@@ -20,12 +20,16 @@
 </div> --><!-- #item-header-avatar -->
 
 <div id="item-header-content">
+	<div id="item-header-avatar">
+		<a href="<?php bp_user_link(); ?>"><?php bp_displayed_user_avatar( 'type=full' ); ?></a>
+	</div><!-- #item-header-avatar -->
+	<div class="noms">
+		<h2>
+			<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_fullname(); ?></a>
+		</h2>
 
-	<h2>
-		<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_fullname(); ?></a>
-	</h2>
-
-	<span class="user-nicename">@<?php bp_displayed_user_username(); ?></span>
+		<span class="user-nicename">@<?php bp_displayed_user_username(); ?></span>
+	</div>
 	<span class="activity"><?php bp_last_activity( bp_displayed_user_id() ); ?></span>
 
 	<?php do_action( 'bp_before_member_header_meta' ); ?>
