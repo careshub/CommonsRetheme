84225c646b24dff4a4b7e3c9d575c843ea0a2614
Michael Barbaro <barbarom@missouri.edu>
2013-03-29 16:27:15 -0500

    removed unneccesary sharing radio buttons

--- includes/SA_Resources.php
+++ includes/SA_Resources.php
@@ -39,39 +39,7 @@ function SA_resources_init()
         'map_meta_cap'    => true
 	); 
 	register_post_type('saresources',$args);
-}
-
-
-//Building the input form in the WordPress admin area
-add_action( 'admin_init', 'resource_meta_box_add' );
-function resource_meta_box_add(){
-	 add_meta_box( 'resource_meta_box', 'Resource Sharing', 'resource_meta_box', 'SA Resources', 'normal', 'high');     
-}
-
-function resource_meta_box()
-{
-    global $post;
-    $custom = get_post_custom($post->ID);   
-	$resourcesharing = $custom["sa_sharing"][0]; 
-
-?>  
-	
-	<div style="padding-left:20px;">
-	<input type="radio" name="sa_sharing" value="curators"            
-			<?php 
-                if ($resourcesharing == "curators") {
-                    echo " checked";
-                }
-           ?>> Curators<br>
-	<input type="radio" name="sa_sharing" value="public"
-			<?php 
-                if ($resourcesharing == "public") {
-                    echo " checked";
-                }
-           ?>> Public<br>
-	</div>
-	
-<?php     
+  
 
 
 
@@ -79,27 +47,7 @@ function resource_meta_box()
 }
 
 
- add_action( 'save_post', 'saresource_save' );
- function saresource_save() { 
- 
-   global $post;
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-      return;
 
-     if ($post->post_type == 'saresources') {      
-     
-	   save_res_field("sa_sharing");	   
-	   
-     }
- }
- function save_res_field($event_field) {
-    global $post;
-    if(isset($_POST[$event_field])) {
-        update_post_meta($post->ID, $event_field, $_POST[$event_field]);
-    } else{
-        delete_post_meta($post->ID, $event_field);
-    }
- }
 
 ?>
 
