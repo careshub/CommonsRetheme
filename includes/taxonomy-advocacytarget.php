6dbb6f84814efd78b6ed350275c3a6a57fcf891a
Michael Barbaro <barbarom@missouri.edu>
2013-04-08 09:38:17 -0500

    added 'sa_' prefix to taxonomy

--- includes/taxonomy-advocacytarget.php
+++ includes/taxonomy-advocacytarget.php
@@ -4,21 +4,21 @@ add_action( 'init', 'register_taxonomy_advocacy_targets' );
 function register_taxonomy_advocacy_targets() {
 
     $labels = array( 
-        'name' => _x( 'Advocacy Targets', 'advocacy_targets' ),
-        'singular_name' => _x( 'Advocacy Target', 'advocacy_target' ),
-        'search_items' => _x( 'Search Advocacy Targets', 'advocacy_targets' ),
-        'popular_items' => _x( 'Popular Advocacy Targets', 'advocacy_targets' ),
-        'all_items' => _x( 'All Advocacy Targets', 'advocacy_targets' ),
-        'parent_item' => _x( 'Parent Advocacy Target', 'advocacy_targets' ),
-        'parent_item_colon' => _x( 'Parent Advocacy Target:', 'advocacy_targets' ),
-        'edit_item' => _x( 'Edit Advocacy Target', 'advocacy_targets' ),
-        'update_item' => _x( 'Update Advocacy Target', 'advocacy_targets' ),
-        'add_new_item' => _x( 'Add New Advocacy Target', 'advocacy_targets' ),
-        'new_item_name' => _x( 'New Advocacy Target', 'advocacy_targets' ),
-        'separate_items_with_commas' => _x( 'Separate advocacy targets with commas', 'advocacy_targets' ),
-        'add_or_remove_items' => _x( 'Add or remove Advocacy Targets', 'advocacy_targets' ),
-        'choose_from_most_used' => _x( 'Choose from most used Advocacy Targets', 'advocacy_targets' ),
-        'menu_name' => _x( 'Advocacy Targets', 'advocacy_targets' ),
+        'name' => _x( 'Advocacy Targets', 'sa_advocacy_targets' ),
+        'singular_name' => _x( 'Advocacy Target', 'sa_advocacy_target' ),
+        'search_items' => _x( 'Search Advocacy Targets', 'sa_advocacy_targets' ),
+        'popular_items' => _x( 'Popular Advocacy Targets', 'sa_advocacy_targets' ),
+        'all_items' => _x( 'All Advocacy Targets', 'sa_advocacy_targets' ),
+        'parent_item' => _x( 'Parent Advocacy Target', 'sa_advocacy_targets' ),
+        'parent_item_colon' => _x( 'Parent Advocacy Target:', 'sa_advocacy_targets' ),
+        'edit_item' => _x( 'Edit Advocacy Target', 'sa_advocacy_targets' ),
+        'update_item' => _x( 'Update Advocacy Target', 'sa_advocacy_targets' ),
+        'add_new_item' => _x( 'Add New Advocacy Target', 'sa_advocacy_targets' ),
+        'new_item_name' => _x( 'New Advocacy Target', 'sa_advocacy_targets' ),
+        'separate_items_with_commas' => _x( 'Separate advocacy targets with commas', 'sa_advocacy_targets' ),
+        'add_or_remove_items' => _x( 'Add or remove Advocacy Targets', 'sa_advocacy_targets' ),
+        'choose_from_most_used' => _x( 'Choose from most used Advocacy Targets', 'sa_advocacy_targets' ),
+        'menu_name' => _x( 'Advocacy Targets', 'sa_advocacy_targets' ),
     );
 
     $args = array( 
@@ -34,5 +34,5 @@ function register_taxonomy_advocacy_targets() {
         'query_var' => true
     );
 
-    register_taxonomy( 'advocacy_targets', array('sapolicies'), $args );
+    register_taxonomy( 'sa_advocacy_targets', array('sapolicies'), $args );
 }
