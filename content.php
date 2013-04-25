bb1f5d616ee7e188e75c47a352917dc77af58bae
David Cavins <david.cavins@gmail.com>
2013-04-25 11:39:06 -0500

    Category and Tag page refinements

--- content.php
+++ content.php
@@ -15,10 +15,15 @@
 		</div>
 		<?php endif; ?>
 		<header class="entry-header">
-			<?php the_post_thumbnail(); ?>
 			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+				<?php //the_post_thumbnail(); ?>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
+				<?php if ( has_post_thumbnail()) : ?>
+				   	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+				   	<?php the_post_thumbnail('post-thumbnail'); ?>
+				   	</a>
+			   	<?php endif; ?>
 			<h1 class="entry-title">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
