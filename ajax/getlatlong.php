9ad995748042981eb52a828921d07fcbac6d33f1
Michael Barbaro <barbarom@missouri.edu>
2013-04-30 16:37:19 -0500

    Changed wp-load url to work on dev

--- ajax/getlatlong.php
+++ ajax/getlatlong.php
@@ -1,6 +1,7 @@
 <?php
+
 define('WP_USE_THEMES', false);
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wordpress/wp-load.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 
 $finalgeog = $_POST['finalgeog'];
 $geog = $_POST['geog'];
