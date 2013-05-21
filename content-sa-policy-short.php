2d9307e5655388913c9ebaf07fd12c2baebe749d
David Cavins <david.cavins@gmail.com>
2013-05-21 10:19:06 -0500

    Header clean-up

--- content-sa-policy-short.php
+++ content-sa-policy-short.php
@@ -54,13 +54,11 @@
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 				</h2>
 				<?php //echo "<br />"; ?>
-				<p class="location">
-					<?php //echo $location; 
+				<p class="location"><?php //echo $location; 
 						if (function_exists('salud_the_location')) {
 							salud_the_location();
 						}
-					?>
-				</p>
+					?></p>
 				<div class="meter-box clear">
 					<p>This policy is <?php echo $progress_label; ?>.
 					<!-- <div class="meter">
