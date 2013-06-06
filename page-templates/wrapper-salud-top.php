5473243f6c95c6ad2d0d3ed4c4df843ec414242c
eloiespanol <espanole@missouri.edu>
2013-06-06 15:48:48 -0500

    Change in subtitle text required by Cliff

--- page-templates/wrapper-salud-top.php
+++ page-templates/wrapper-salud-top.php
@@ -1,7 +1,7 @@
 <div class="salud-header clear">
 	<a href="#" class="logo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/SA-logox200.png" class=""></a>
 	<h1>Salud America! <br /><span class="salud-tagline">Growing Healthy Change</span></h1>
-	<h3>Get involved in reducing latino childhood obesity.</h3>
+	<h3>Your portal to reduce Latino child obesity.</h3>
 	<div class="sa-kids-photo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/sa-kids-335.png"></div>
 
 </div>
