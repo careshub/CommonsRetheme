88cdd078508cf518898761da24b679b9eb069fc5
Michael Barbaro <barbarom@missouri.edu>
2013-06-27 16:01:18 -0500

    changed to use Google geocoder

--- ajax/getlatlong.php
+++ ajax/getlatlong.php
@@ -7,43 +7,65 @@ $finalgeog = $_POST['finalgeog'];
 $geog = $_POST['geog'];
 $st = $_POST['state'];
 
-if ($geog == "City"){
-    $myterm = get_term_by('slug', $finalgeog . "-" . $st, 'geographies');
-} else {
-    $myterm = get_term_by('name', $finalgeog, 'geographies');
+if ($geog != "National") {
+$geogstr = $finalgeog . " " . $st;
+} 
+else
+{
+$geogstr = $finalgeog;
 }
 
-$termdesc = term_description( $myterm->term_id, 'geographies' );
 
-			if (strlen($termdesc)>5){
-				$pos = strpos($termdesc, ",");
-				if ($pos === false){
+// if ($geog == "City") {
+    // $myterm = get_term_by('slug', $finalgeog . "-" . $st, 'geographies');
+// } else {
+    // $myterm = get_term_by('name', $finalgeog, 'geographies');
+// }
+
+//$termdesc = term_description( $myterm->term_id, 'geographies' );
+
+			// if (strlen($termdesc)>5){
+				// $pos = strpos($termdesc, ",");
+				// if ($pos === false){
 					
-				} else {
-					$parts = explode(",", $termdesc);                               
-					$lat1 = (float) trim(str_replace("<p>", "", $parts[0]));
-					$long1 = (float) trim($parts[1]);
-					$lat2 = (float) trim($parts[2]);
-					$long2 = (float) trim(str_replace("</p>", "", $parts[3]));
-                    $avglat=($lat1+$lat2)/2;
-                    $avglong=($long1+$long2)/2;
-                    // printf('<input id="sa_latitude" name="sa_latitude" value="' . $avglat . '" /><input id="sa_longitude" name="sa_longitude" value="' . $avglong . '" />');
-					$coordinates = array( 
-						'latitude' => $avglat,
-						'longitude' => $avglong
-						);
-					printf( json_encode($coordinates) );
-				}
-			} 
+				// } else {
+					// $parts = explode(",", $termdesc);                               
+					// $lat1 = (float) trim(str_replace("<p>", "", $parts[0]));
+					// $long1 = (float) trim($parts[1]);
+					// $lat2 = (float) trim($parts[2]);
+					// $long2 = (float) trim(str_replace("</p>", "", $parts[3]));
+                    // $avglat=($lat1+$lat2)/2;
+                    // $avglong=($long1+$long2)/2;
+                    //printf('<input id="sa_latitude" name="sa_latitude" value="' . $avglat . '" /><input id="sa_longitude" name="sa_longitude" value="' . $avglong . '" />');
+					// $coordinates = array( 
+						// 'latitude' => $avglat,
+						// 'longitude' => $avglong
+						// );
+					// printf( json_encode($coordinates) );
+				// }
+			// } 
 
-//        if(strlen($finalgeog)>3) {      
-//                $loc = $finalgeog;  
-//                $loc2 = str_replace(" ","%20",$loc);
-//                $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address=' . $loc2 . '&sensor=false');
-//                $output = json_decode($geocode);
-//                $lat = $output->results[0]->geometry->location->lat;
-//                $long = $output->results[0]->geometry->location->lng; 
-//                printf('<input type="hidden" id="sa_latitude" name="sa_latitude" value="' . $lat . '" /><input type="hidden" id="sa_longitude" name="sa_longitude" value="' . $long . '" />');
-//
-//                
-//            }
+       if(strlen($geogstr)>3) {      
+               $loc = $geogstr;  
+               $loc2 = str_replace(" ","%20",$loc);
+               $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address=' . $loc2 . '&sensor=false');
+               $output = json_decode($geocode);
+               $lt = $output->results[0]->geometry->location->lat;
+               $lg = $output->results[0]->geometry->location->lng;
+				$nelat = $output->results[0]->geometry->viewport->northeast->lat;
+				$nelng = $output->results[0]->geometry->viewport->northeast->lng;
+				$swlat = $output->results[0]->geometry->viewport->southwest->lat;
+				$swlng = $output->results[0]->geometry->viewport->southwest->lng;
+					$coordinates = array( 
+						'latitude' => $lt,
+						'longitude' => $lg,
+						'nelat' => $nelat,
+						'nelng' => $nelng,
+						'swlat' => $swlat,
+						'swlng' => $swlng
+						);
+					printf( json_encode($coordinates) );			   
+               //printf('<input type="hidden" id="sa_latitude" name="sa_latitude" value="' . $lt . '" /><input type="hidden" id="sa_longitude" name="sa_longitude" value="' . $lg . '" />');
+
+               
+           }
