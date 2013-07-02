243d76a17efcb5eb8eade39bdb90dd9156d7c7c1
David Cavins <david.cavins+git@gmail.com>
2013-07-02 11:02:18 -0500

    Set policy geography coords on page load
    If any of the coord inputs are empty, run the lat/long fetcher.
    Also populate final_geog if it's empty and selected_geog has a value.

--- includes/SA_Policies.php
+++ includes/SA_Policies.php
@@ -229,12 +229,14 @@ function sa_geog_meta_box()
                     
                 </select>
                 <input type="hidden" id="sa_finalgeog" value="<?php echo $selectedgeog; ?>" name="sa_finalgeog" />
-                <input type="hidden" id="sa_latitude" value="<?php echo $sa_latitude; ?>" name="sa_latitude">
-                <input type="hidden" id="sa_longitude" value="<?php echo $sa_longitude; ?>" name="sa_longitude">
-				 				<input type="hidden" id="sa_nelat" value="<?php echo $sa_nelat; ?>" name="sa_nelat">
-        				<input type="hidden" id="sa_nelng" value="<?php echo $sa_nelng; ?>" name="sa_nelng">
-        				<input type="hidden" id="sa_swlat" value="<?php echo $sa_swlat; ?>" name="sa_swlat">
-        				<input type="hidden" id="sa_swlng" value="<?php echo $sa_swlng; ?>" name="sa_swlng">
+                <div id="geography_coords">
+                  <input type="hidden" id="sa_latitude" value="<?php echo $sa_latitude; ?>" name="sa_latitude">
+                  <input type="hidden" id="sa_longitude" value="<?php echo $sa_longitude; ?>" name="sa_longitude">
+                  <input type="hidden" id="sa_nelat" value="<?php echo $sa_nelat; ?>" name="sa_nelat">
+                  <input type="hidden" id="sa_nelng" value="<?php echo $sa_nelng; ?>" name="sa_nelng">
+                  <input type="hidden" id="sa_swlat" value="<?php echo $sa_swlat; ?>" name="sa_swlat">
+                  <input type="hidden" id="sa_swlng" value="<?php echo $sa_swlng; ?>" name="sa_swlng">
+                </div>
             </div>            
         </div>
 </div>
@@ -397,15 +399,27 @@ function refresh_sa_policy_stage_vis_setting() {
 </script>
 
 <script type="text/javascript">
-    //Handle the geography input form
+//Handle the geography input form
 jQuery(document).ready(function(){
       //On page load, update the inputs that are enabled
         refresh_sa_policy_enable_geog_inputs();
-		//TODO Refresh lat/longs on page load if they don't exist
-		//TODO Refresh bounding box coordinates (nelat,nelng,swlat,swlng) if they don't exist
 
-		//TODO Refresh lat/longs on page load if they don't exist
-		//TODO Refresh bounding box coordinates (nelat,nelng,swlat,swlng) if they don't exist
+      //If a geography has been selected, but not copied over to #final_geog, do it.
+      //TODO: Why are we using final_geog again?
+        if ( ( jQuery('#sa_finalgeog').val() == '' ) && ( jQuery('#sa_selectedgeog').val() !== '' )  ) {
+          jQuery("#sa_finalgeog").val(jQuery("#sa_selectedgeog").val());
+        }
+
+      //Refresh lat/longs on page load if any are empty
+      var emptyCoords = jQuery('#geography_coords input').filter(function() { return this.value == ""; });
+
+      //emptyCoords will return an object of objects if it finds any empty inputs
+      if (emptyCoords.length > 0) {
+          //This function will only run if #sa_finalgeog isn't empty
+          get_sa_geog_lat_lon();
+      } else {
+        // console.log('No need to run the function');
+      }
 
       //On change, refresh the option list and option list visibility
       //The page load setup is handled via php, so the js only has to handle the updates
