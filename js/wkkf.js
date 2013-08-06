a60fc28ab28f92ad8e305dbf7158e33cc355ab47
Michael Barbaro <barbarom@missouri.edu>
2013-08-06 10:38:56 -0500

    WKKF updated js to account for default page

--- js/wkkf.js
+++ js/wkkf.js
@@ -9,12 +9,25 @@ function GoToPage(baseurl, priplace, page) {
 }
 
 jQuery(document).ready(function(){
-	
-	      jQuery('#pageselector').live( 'change', function() { 			
-            window.location=jQuery('#pageselector').val();         
-          });
+
+		var pg = getParameterByName("pg");			
+		var loc = getParameterByName("loc");
+		var burl = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;		
+		if (!pg) {
+			GoToPage(burl,loc,'context');
+		}
+		jQuery('#pageselector').live( 'change', function() { 			
+		window.location=jQuery('#pageselector').val();         
+		});
 
 });
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
 
 
