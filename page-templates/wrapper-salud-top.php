244c91d12fa42dfb778d4bebea02148e080bb126
eloiespanol <espanole@missouri.edu>
2013-09-02 15:07:15 -0500

    SA_Header_links

--- page-templates/wrapper-salud-top.php
+++ page-templates/wrapper-salud-top.php
@@ -1,7 +1,12 @@
 <div class="salud-header clear">
 	<a href="#" class="logo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/SA-logox200.png" class=""></a>
 	<h1>Salud America! <br /><span class="salud-tagline">Growing Healthy Change</span></h1>
-	<h3>Your portal to reduce Latino child obesity.</h3>
+	<h3>Your portal to reduce Latino child obesity.
+            <a href='http://www.facebook.com/pages/SaludToday/160946931268'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Facebook_Icon.png' style='width:25px; height: 25px; padding-left:120px; vertical-align: middle;' alt='Facebook'></a>
+            <a href='http://twitter.com/saludtoday'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Twitter_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Twitter'></a>
+            <a href='http://www.youtube.com/user/SaludToday'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/YouTube_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Youtube'></a>
+            <a href='http://www.saludtoday.com/blog/'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Blog_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Blog'></a>
+        </h3>
 	<div class="sa-kids-photo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/sa-kids-335.png"></div>
 
 </div>
