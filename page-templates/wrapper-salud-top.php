26ed48b59e82675356ecdc1596faab6ca24a5a32
eloiespanol <espanole@missouri.edu>
2013-09-19 13:10:33 -0500

    SA - Home page kids back on

--- page-templates/wrapper-salud-top.php
+++ page-templates/wrapper-salud-top.php
@@ -11,8 +11,14 @@
 			    <a href='http://www.saludtoday.com/blog/'><img src='http://dev.communitycommons.org/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Blog_Icon.png' alt='Blog'></a>
 		    </div>
 	    </div>
-	<h3 class="screamer saorange">Let's reduce Latino childhood obesity!</h3>
-	<!-- <div class="sa-kids-photo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/sa-kids-335.png"></div> -->
+	<h3 class="screamer saorange">Let's reduce Latino childhood obesity!
+        <a href='http://www.facebook.com/pages/SaludToday/160946931268'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Facebook_Icon.png' style='width:25px; height: 25px; padding-left:330px; vertical-align: middle;' alt='Facebook'></a>
+	<a href='http://twitter.com/saludtoday'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Twitter_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Twitter'></a>
+	<a href='http://www.youtube.com/user/SaludToday'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/YouTube_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Youtube'></a>
+	<a href='http://www.saludtoday.com/blog/'><img src='/wp-content/themes/CommonsRetheme/img/salud_america/Salud_Platform_WebReady_files/Blog_Icon.png' style='width:25px; height: 25px; padding-left:0px; vertical-align: middle;' alt='Blog'></a>
+
+        </h3>
+	<div class="sa-kids-photo"><img src="/wp-content/themes/CommonsRetheme/img/salud_america/sa-kids-335.png"></div>
 
 </div>
 
