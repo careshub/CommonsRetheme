262a02832705d4b4478237432639c5626990a104
David Cavins <david.cavins@gmail.com>
2014-02-04 12:39:03 -0600

    Front page button text mod

--- front-page.php
+++ front-page.php
@@ -56,7 +56,7 @@ $do_not_duplicate = array();
 			// echo '<br />';
 
 		    //$related_tag = $post->tag ?>
-		    <h4 class="clear-none"><a href="/blog/" title="Article archive" class="button">Browse all articles.</a></h4>
+		    <h4 class="clear-none"><a href="/blog/" title="Article archive" class="button">Browse all Commons articles.</a></h4>
 		</div> <!-- end #top-story -->
 
 		<?php endwhile; 
