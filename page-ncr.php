8da884d23a81b2658b7dc14f24870533792303d1
David Cavins <david.cavins@gmail.com>
2014-02-04 19:08:38 -0600

    NCR page updates
    Adding brief descriptions to the link items

--- page-ncr.php
+++ page-ncr.php
@@ -21,7 +21,7 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 
 			<!-- NCR Authors on the Commons -->
-			<div id="tool-group-authors" class="tool-group accent-yellow">
+			<div id="tool-group-authors" class="tool-group accent-green">
 				<header class="entry-header clear">
 					<h1 class="entry-title">NCR Authors on the Commons</h1>
 					<div id="tool-group-header-videos" class="tool-group-header clear">
@@ -34,16 +34,25 @@ get_header(); ?>
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.communitycommons.org/2014/01/national-civic-review/" title="Welcome from Monte Roulier">Welcome by Monte Roulier</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Read about the partnership between Community Commons and the National Civic League</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block guest-voices">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.communitycommons.org/ncr-guest-voices/" title="Guest Voices">Guest Voices</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Engage with others on the ideas behind the Healthy Communities Movement</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block feature-stories">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.communitycommons.org/ncr-features-stories/" title="Featured Stories">Feature Stories</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Discover what the Healthy Communities Movements looks like around the country</p>
+						</div>
 					</div>			
 				</div><!-- End .content-row clear -->
 		    </div> <!-- .tool-group -->
@@ -53,7 +62,7 @@ get_header(); ?>
 					<h1 class="entry-title">NCR Special Issue Part 1</h1>
 					<div id="tool-group-header-videos" class="tool-group-header clear">
 						<img src="http://www.communitycommons.org/wp-content/uploads/2014/02/NCR-tyler-600x200.jpg" class="attachment-full" alt="Photo of Tyler Norris">
-						<p class="tool-group-description">Explore NCR’s Special Issue</p>
+						<p class="tool-group-description">Explore NCR’s Special Issue.</p>
 					</div>
 				</header>
 		  	    <div class="content-row clear">
@@ -61,21 +70,30 @@ get_header(); ?>
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://onlinelibrary.wiley.com/doi/10.1002/ncr.21142/full" title="Introduction by Tyler Norris">Introduction by Tyler Norris</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Read Tyler’s framework of the themes presented in the articles</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block guest-voices">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.communitycommons.org/ncr-table-of-contents-with-synopses/" title="Table of Contents">Table of Contents with Synopses</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Browse the titles and an overview of each article</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block feature-stories">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://onlinelibrary.wiley.com/doi/10.1002/ncr.v102.4/issuetoc" title="Link to the National Civic Review's site">National Civic Review Online Publication</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Access individual abstracts, articles, and citations</p>
+						</div>
 					</div>			
 				</div><!-- End .content-row clear -->
 			</div>
 			<!-- NCL: All American City Award -->
-			<div id="tool-group-aac-award" class="tool-group accent-green">
+			<div id="tool-group-aac-award" class="tool-group accent-yellow">
 				<header class="entry-header clear">
 					<h1 class="entry-title">NCR: All-America City Award</h1>
 					<div id="tool-group-header-aac" class="tool-group-header clear">
@@ -88,16 +106,25 @@ get_header(); ?>
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.ncl.org/index.php?option=com_content&view=article&id=102&Itemid=179" title="Background Information">Background Information</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Learn about the deep and distinguished history of the awards</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block aac-2014">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.ncl.org/index.php?option=com_content&view=article&id=186&Itemid=226" title="All-America cities 2014">All-America Cities 2014</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Explore what it takes to become an All-America City</p>
+						</div>
 					</div>
 					<div class="tool-group-tool third-block aac-application">
 						<header class="entry-header clear">
 							<h3 class="entry-title"><a href="http://www.ncl.org/index.php?option=com_jforms&view=form&id=2&Itemid=196" title="All-America City Application">Application</a></h3>
 						</header>
+						<div class="entry-content">
+							<p>Access the application materials and begin your journey</p>
+						</div>
 					</div>			
 				</div><!-- End .content-row clear -->
 		    </div> <!-- .tool-group -->
