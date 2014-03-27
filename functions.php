58545b01160e7ff49e4b9864baeab3bb3c90ba70
David Cavins <david.cavins@gmail.com>
2014-03-27 15:00:20 -0500

    Cache-busting!

--- functions.php
+++ functions.php
@@ -143,7 +143,7 @@ function custom_childtheme_stylesheet_load(){
           'commons_retheme_stylesheet',
           get_stylesheet_uri(),
           false,
-          0.32
+          0.33
       );
   wp_enqueue_style( 'commons_retheme_stylesheet' );
 }
@@ -155,7 +155,7 @@ function commons_ie_stylesheet_load(){
             'commons_ie_stylesheet',
             get_stylesheet_directory_uri() . '/style-ie.css',
             false,
-            0.2
+            0.21
         );
     wp_enqueue_style( 'commons_ie_stylesheet' );
     $wp_styles->add_data( 'commons_ie_stylesheet', 'conditional', 'lt IE 9' );
