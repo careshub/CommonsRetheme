6172f1dbdb384635c2efe59697b450170ef0dce9
David Cavins <david.cavins+git@gmail.com>
2014-03-31 11:22:48 -0500

    Correction to IE stylesheet location

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 <?php wp_head(); ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<link rel='stylesheet' id='commons_ie_stylesheet-css'  href='http://commonsdev.local/wp-content/themes/CommonsRetheme/style-ie.css?ver=0.21' type='text/css' media='all' />
+<link rel='stylesheet' id='commons_ie_stylesheet-css'  href='<?php echo get_stylesheet_directory_uri(); ?>/style-ie.css?ver=0.32' type='text/css' media='all' />
 <![endif]-->
 </head>
 
