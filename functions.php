6886447e461209bfc3d955cfc78ad90993c5b48a
David Cavins <david.cavins@gmail.com>
2014-10-14 12:31:02 -0500

    Add accessibility styles for screen reader text
    bump version number

--- functions.php
+++ functions.php
@@ -64,7 +64,7 @@ function custom_childtheme_stylesheet_load(){
           'commons_retheme_stylesheet',
           get_stylesheet_uri(),
           false,
-          0.38
+          0.40
       );
   wp_enqueue_style( 'commons_retheme_stylesheet' );
 }
@@ -76,7 +76,7 @@ function commons_ie_stylesheet_load(){
             'commons_ie_stylesheet',
             get_stylesheet_directory_uri() . '/style-ie.css',
             false,
-            0.38
+            0.40
         );
     wp_enqueue_style( 'commons_ie_stylesheet' );
     $wp_styles->add_data( 'commons_ie_stylesheet', 'conditional', 'lt IE 9' );
