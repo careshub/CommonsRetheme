2ea3fd7080edbdf7e13794acc2aee3e2c4adff3f
Michael Barbaro <barbarom@missouri.edu>
2015-01-15 10:31:29 -0600

    JS to clear nums in forms - ODH

--- js/gravity-forms-odh-chc.js
+++ js/gravity-forms-odh-chc.js
@@ -19,6 +19,9 @@ jQuery(document).ready(function($){
 		if( !$("li.sec1_" + val + " input.small").val() ) {
 			$("li.sec1_" + val + " input.small").val(0);
 		}
+		
+		clearInputs("1", val);		
+		
 		if ( !$("li.sec1_" + val + "_T input.small").val() ) {
 			var ttl = parseInt($("li.sec1_" + val + "_1 input.small").val()) + parseInt($("li.sec1_" + val + "_2 input.small").val()) + parseInt($("li.sec1_" + val + "_3 input.small").val()) + parseInt($("li.sec1_" + val + "_4 input.small").val());
 			$("li.sec1_" + val + "_T input.small").val(ttl);		
@@ -35,21 +38,6 @@ jQuery(document).ready(function($){
 	
 	$("li.sec1_19_3 input.small").trigger('blur');
 	
-	function calcImpacted(val) {
-			if (val > 17 && val < 22) {
-				col1_tot = parseInt($("li.sec1_18_1 input.small").val()) + parseInt($("li.sec1_19_1 input.small").val()) + parseInt($("li.sec1_20_1 input.small").val()) + parseInt($("li.sec1_21_1 input.small").val());
-				col2_tot = parseInt($("li.sec1_18_2 input.small").val()) + parseInt($("li.sec1_19_2 input.small").val()) + parseInt($("li.sec1_20_2 input.small").val()) + parseInt($("li.sec1_21_2 input.small").val());
-				col3_tot = parseInt($("li.sec1_18_3 input.small").val()) + parseInt($("li.sec1_19_3 input.small").val()) + parseInt($("li.sec1_20_3 input.small").val()) + parseInt($("li.sec1_21_3 input.small").val());
-				col4_tot = parseInt($("li.sec1_18_4 input.small").val()) + parseInt($("li.sec1_19_4 input.small").val()) + parseInt($("li.sec1_20_4 input.small").val()) + parseInt($("li.sec1_21_4 input.small").val());
-				col5_tot = parseInt($("li.sec1_18_T input.small").val()) + parseInt($("li.sec1_19_T input.small").val()) + parseInt($("li.sec1_20_T input.small").val()) + parseInt($("li.sec1_21_T input.small").val());
-			}		
-			
-			$("li.sec1_eventQ1_T input.small").val(col1_tot);
-			$("li.sec1_eventQ2_T input.small").val(col2_tot);
-			$("li.sec1_eventQ3_T input.small").val(col3_tot);
-			$("li.sec1_eventQ4_T input.small").val(col4_tot);
-			$("li.sec1_eventAll_T input.small").val(col5_tot);	
-	}
 	
 	//Section II
 	var arrSec2 = new Array();
@@ -61,6 +49,9 @@ jQuery(document).ready(function($){
 		if( !$("li.sec2_" + val2 + " input.small").val() ) {
 			$("li.sec2_" + val2 + " input.small").val(0);
 		}
+		
+		clearInputs("2", val2);
+		
 		if ( !$("li.sec2_" + val2 + "_T input.small").val() ) {
 			var ttl2 = parseInt($("li.sec2_" + val2 + "_1 input.small").val()) + parseInt($("li.sec2_" + val2 + "_2 input.small").val()) + parseInt($("li.sec2_" + val2 + "_3 input.small").val()) + parseInt($("li.sec2_" + val2 + "_4 input.small").val());
 			$("li.sec2_" + val2 + "_T input.small").val(ttl2);		
@@ -85,6 +76,9 @@ jQuery(document).ready(function($){
 		if( !$("li.sec3_" + val3 + " input.small").val() ) {
 			$("li.sec3_" + val3 + " input.small").val(0);
 		}
+		
+		clearInputs("3", val3);
+		
 		if ( !$("li.sec3_" + val3 + "_T input.small").val() ) {
 			var ttl3 = parseInt($("li.sec3_" + val3 + "_1 input.small").val()) + parseInt($("li.sec3_" + val3 + "_2 input.small").val()) + parseInt($("li.sec3_" + val3 + "_3 input.small").val()) + parseInt($("li.sec3_" + val3 + "_4 input.small").val());
 			$("li.sec3_" + val3 + "_T input.small").val(ttl3);		
@@ -105,6 +99,9 @@ jQuery(document).ready(function($){
 		if( !$("li.sec4_" + val4 + " input.small").val() ) {
 			$("li.sec4_" + val4 + " input.small").val(0);
 		}
+		
+		clearInputs("4", val4);
+		
 		if ( !$("li.sec4_" + val4 + "_T input.small").val() ) {
 			var ttl4 = parseInt($("li.sec4_" + val4 + "_1 input.small").val()) + parseInt($("li.sec4_" + val4 + "_2 input.small").val()) + parseInt($("li.sec4_" + val4 + "_3 input.small").val()) + parseInt($("li.sec4_" + val4 + "_4 input.small").val());
 			$("li.sec4_" + val4 + "_T input.small").val(ttl4);		
@@ -125,6 +122,9 @@ jQuery(document).ready(function($){
 		if( !$("li.sec5_" + val5 + " input.small").val() ) {
 			$("li.sec5_" + val5 + " input.small").val(0);
 		}
+		
+		clearInputs("5", val5);
+		
 		if ( !$("li.sec5_" + val5 + "_T input.small").val() ) {
 			var ttl5 = parseInt($("li.sec5_" + val5 + "_1 input.small").val()) + parseInt($("li.sec5_" + val5 + "_2 input.small").val()) + parseInt($("li.sec5_" + val5 + "_3 input.small").val()) + parseInt($("li.sec5_" + val5 + "_4 input.small").val());
 			$("li.sec5_" + val5 + "_T input.small").val(ttl5);		
@@ -133,6 +133,74 @@ jQuery(document).ready(function($){
 			var ttl5 = parseInt($("li.sec5_" + val5 + "_1 input.small").val()) + parseInt($("li.sec5_" + val5 + "_2 input.small").val()) + parseInt($("li.sec5_" + val5 + "_3 input.small").val()) + parseInt($("li.sec5_" + val5 + "_4 input.small").val());
 			$("li.sec5_" + val5 + "_T input.small").val(ttl5);		
 		});		
-	});			
+	});	
+
+	function calcImpacted(val) {
+			if (val > 17 && val < 22) {
+				col1_tot = parseInt($("li.sec1_18_1 input.small").val()) + parseInt($("li.sec1_19_1 input.small").val()) + parseInt($("li.sec1_20_1 input.small").val()) + parseInt($("li.sec1_21_1 input.small").val());
+				col2_tot = parseInt($("li.sec1_18_2 input.small").val()) + parseInt($("li.sec1_19_2 input.small").val()) + parseInt($("li.sec1_20_2 input.small").val()) + parseInt($("li.sec1_21_2 input.small").val());
+				col3_tot = parseInt($("li.sec1_18_3 input.small").val()) + parseInt($("li.sec1_19_3 input.small").val()) + parseInt($("li.sec1_20_3 input.small").val()) + parseInt($("li.sec1_21_3 input.small").val());
+				col4_tot = parseInt($("li.sec1_18_4 input.small").val()) + parseInt($("li.sec1_19_4 input.small").val()) + parseInt($("li.sec1_20_4 input.small").val()) + parseInt($("li.sec1_21_4 input.small").val());
+				col5_tot = parseInt($("li.sec1_18_T input.small").val()) + parseInt($("li.sec1_19_T input.small").val()) + parseInt($("li.sec1_20_T input.small").val()) + parseInt($("li.sec1_21_T input.small").val());
+			}		
+			
+			$("li.sec1_eventQ1_T input.small").val(col1_tot);
+			$("li.sec1_eventQ2_T input.small").val(col2_tot);
+			$("li.sec1_eventQ3_T input.small").val(col3_tot);
+			$("li.sec1_eventQ4_T input.small").val(col4_tot);
+			$("li.sec1_eventAll_T input.small").val(col5_tot);	
+	}	
+	
+	function clearInputs(secNo, val) {
+		var default_value1 = $("li.sec" + secNo + "_" + val + "_1 input.small").val();	
+		var default_value2 = $("li.sec" + secNo + "_" + val + "_2 input.small").val();
+		var default_value3 = $("li.sec" + secNo + "_" + val + "_3 input.small").val();
+		var default_value4 = $("li.sec" + secNo + "_" + val + "_4 input.small").val();
+		
+		$("li.sec" + secNo + "_" + val + "_1 input.small").focus(function() {		
+			if ($("li.sec" + secNo + "_" + val + "_1 input.small").val() == default_value1) {
+				$("li.sec" + secNo + "_" + val + "_1 input.small").val("");
+			}			
+		});
+		$("li.sec" + secNo + "_" + val + "_1 input.small").blur(function() {		
+			if (!$("li.sec" + secNo + "_" + val + "_1 input.small").val()) {
+				$("li.sec" + secNo + "_" + val + "_1 input.small").val(default_value1);
+			}
+		});
+
+		$("li.sec" + secNo + "_" + val + "_2 input.small").focus(function() {		
+			if ($("li.sec" + secNo + "_" + val + "_2 input.small").val() == default_value2) {
+				$("li.sec" + secNo + "_" + val + "_2 input.small").val("");
+			}			
+		});
+		$("li.sec" + secNo + "_" + val + "_2 input.small").blur(function() {		
+			if (!$("li.sec" + secNo + "_" + val + "_2 input.small").val()) {
+				$("li.sec" + secNo + "_" + val + "_2 input.small").val(default_value2);
+			}
+		});
+
+		$("li.sec" + secNo + "_" + val + "_3 input.small").focus(function() {		
+			if ($("li.sec" + secNo + "_" + val + "_3 input.small").val() == default_value3) {
+				$("li.sec" + secNo + "_" + val + "_3 input.small").val("");
+			}			
+		});
+		$("li.sec" + secNo + "_" + val + "_3 input.small").blur(function() {		
+			if (!$("li.sec" + secNo + "_" + val + "_3 input.small").val()) {
+				$("li.sec" + secNo + "_" + val + "_3 input.small").val(default_value3);
+			}
+		});
+
+		$("li.sec" + secNo + "_" + val + "_4 input.small").focus(function() {		
+			if ($("li.sec" + secNo + "_" + val + "_4 input.small").val() == default_value4) {
+				$("li.sec" + secNo + "_" + val + "_4 input.small").val("");
+			}			
+		});
+		$("li.sec" + secNo + "_" + val + "_4 input.small").blur(function() {		
+			if (!$("li.sec" + secNo + "_" + val + "_4 input.small").val()) {
+				$("li.sec" + secNo + "_" + val + "_4 input.small").val(default_value4);
+			}
+		});		
+	}
+	
 
 });
