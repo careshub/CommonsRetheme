1746f9f5defb26719be23dc7cbf5bcf5d5b824ad
David Cavins <david.cavins@gmail.com>
2015-01-29 11:07:48 -0600

    Bump stylesheet version number

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ function custom_childtheme_stylesheet_load(){
           'commons_retheme_stylesheet',
           get_stylesheet_uri(),
           false,
-          0.40
+          0.45
       );
   wp_enqueue_style( 'commons_retheme_stylesheet' );
 }
@@ -74,7 +74,7 @@ function commons_ie_stylesheet_load(){
             'commons_ie_stylesheet',
             get_stylesheet_directory_uri() . '/style-ie.css',
             false,
-            0.40
+            0.45
         );
     wp_enqueue_style( 'commons_ie_stylesheet' );
     $wp_styles->add_data( 'commons_ie_stylesheet', 'conditional', 'lt IE 9' );
