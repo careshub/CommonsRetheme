4377f12a941f406e106b13a017b941ef02148360
David Cavins <david.cavins@gmail.com>
2015-02-18 12:44:14 -0600

    Salud text change

--- page-templates/salud-america.php
+++ page-templates/salud-america.php
@@ -13,7 +13,7 @@ get_header(); ?>
 				<div class="notice" style="border-left:8px solid red;background-color:#F3F3F3;padding: 1px 2em 1em;">
 					<h4 style="color:black;margin-bottom:.5em;"><a href="<?php echo home_url( 'salud-america-video-contest' ); ?>" style="text-decoration:none;color:black"><span class="uppercase" style="text-transform:uppercase; color: red;">Vote &amp; Win: </span>&ensp;Pick Your Fave #SaludHeroes of Food by 2/25/15 &amp; Win a Prize!</a></h4>
                     <a href="<?php echo home_url( 'salud-america-video-contest' ); ?>" class="button">Vote now</a>
-                    <h4 style="color:black;margin-top: 1em;margin-bottom:.5em;"><a href="<?php echo home_url( 'salud-america/tweetchat' ); ?>" style="text-decoration:none;color:black"><span class="uppercase" style="text-transform:uppercase; color: red;">Tweetchat 2/17:</span>&ensp;&ldquo;Education + Health: Empowering the Latino Community&rdquo;</a></h4>
+                    <h4 style="color:black;margin-top: 1em;margin-bottom:.5em;"><a href="<?php echo home_url( 'salud-america/tweetchat' ); ?>" style="text-decoration:none;color:black"><span class="uppercase" style="text-transform:uppercase; color: red;">Tweetchat 2/24:</span>&ensp;&ldquo;How Latina Moms Can Fight Junk Food Marketing&rdquo;</a></h4>
                     <a href="https://twitter.com/SaludToday" target="_blank" class="button" >Follow the conversation</a>&emsp;<a href="<?php echo home_url( 'salud-america/tweetchat' ); ?>" class="button">Learn more</a>
 				</div>
 				<h3 class="screamer sagreen">How can you fight Latino childhood obesity in your area?</h2>
