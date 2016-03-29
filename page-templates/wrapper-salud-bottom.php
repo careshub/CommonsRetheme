300989b425d762a2c2773520b1649afb42b30a9a
David Cavins <david.cavins@gmail.com>
2016-03-29 09:10:07 -0500

    Clean up: Remove Salud America template parts.
    These template parts are no longer needed since moving SA to a group.

--- page-templates/wrapper-salud-bottom.php
+++
@@ -1,7 +0,0 @@
-		<div class="salud-footer">	
-			<p>Salud America!  is a national online network of researchers, community group leaders, decision-makers, and members of the public working together to support healthy policy and environmental changes that can help reverse obesity among Latino children.</p>
-			<p>The network, funded by the Robert Wood Johnson Foundation, is a project of <a href="http://ihpr.uthscsa.edu/"> the Institute for Health Promotion Research (IHPR)</a> at <a href="http://uthscsa.edu/">The UT Health Science Center at San Antonio</a>.</p>
-                        <p>Policies, comments, external links, and contributed stories and images are not affiliated with Salud America!, RWJF, or The UT Health Science Center at San Antonio, nor do they necessarily reflect the views of or endorsement by these organizations.</p>
-                        <a href="http://http://www.rwjf.org/"><img class="alignright" src="/wp-content/themes/CommonsRetheme/img/salud_america/logo-rwjf_small.png" ></a>
-		</div>
-	</div><!-- #primary -->
